256127e11db5ffba7861d891b03ff2bc80c93b90
terryaki75 <chanterry85@gmail.com>
2021-12-10 17:38:30 +0100

    Ajout Inscription

--- migrations/Version20211210160455.php
+++ migrations/Version20211210160455.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211210102709 extends AbstractMigration
+final class Version20211210160455 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,19 +21,21 @@ final class Version20211210102709 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE produit_categorie (produit_id INT NOT NULL, categorie_id INT NOT NULL, INDEX IDX_CDEA88D8F347EFB (produit_id), INDEX IDX_CDEA88D8BCF5E72D (categorie_id), PRIMARY KEY(produit_id, categorie_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE produit_categorie ADD CONSTRAINT FK_CDEA88D8F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE produit_categorie ADD CONSTRAINT FK_CDEA88D8BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE utilisateur ADD adresse_id INT NOT NULL');
-        $this->addSql('ALTER TABLE utilisateur ADD CONSTRAINT FK_1D1C63B34DE7DC5C FOREIGN KEY (adresse_id) REFERENCES adresse (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_1D1C63B34DE7DC5C ON utilisateur (adresse_id)');
+        $this->addSql('ALTER TABLE adresse ADD user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE adresse ADD CONSTRAINT FK_C35F0816A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C35F0816A76ED395 ON adresse (user_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE adresse DROP FOREIGN KEY FK_C35F0816A76ED395');
         $this->addSql('DROP TABLE produit_categorie');
-        $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B34DE7DC5C');
-        $this->addSql('DROP INDEX UNIQ_1D1C63B34DE7DC5C ON utilisateur');
-        $this->addSql('ALTER TABLE utilisateur DROP adresse_id');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP INDEX UNIQ_C35F0816A76ED395 ON adresse');
+        $this->addSql('ALTER TABLE adresse DROP user_id');
     }
 }
