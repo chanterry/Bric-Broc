f5389d21f78dfc557978921895b7c6cadb04936f
terryaki75 <chanterry85@gmail.com>
2021-12-13 17:21:25 +0100

    ajout panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\CartFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -15,6 +18,26 @@ class CartController extends AbstractController
     {
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
+        ]);
+    }
+
+    /**
+     * @Route("/account", name="account")
+     */
+    public function cart_account(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(CartFormType::class, $user);
+        $form ->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+        
+        return $this->render('cart/account.html.twig', [
+            'CartForm' => $form->createView(),
         ]);
     }
 }
