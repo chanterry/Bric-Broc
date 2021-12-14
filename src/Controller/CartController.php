0a53f6847ef083425c0d92ccf7855023d17fb01e
terryaki75 <chanterry85@gmail.com>
2021-12-14 10:36:45 +0100

    ajout panier adresse

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Adresse;
 use App\Form\CartFormType;
+use App\Form\CartAddressType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\Mime\Address;
 
 class CartController extends AbstractController
 {
@@ -38,6 +41,26 @@ class CartController extends AbstractController
         
         return $this->render('cart/account.html.twig', [
             'CartForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/address", name="cart_address")
+     */
+    public function cart_address(Request $request): Response
+    {
+        $address = new Adresse();
+        $form = $this->createForm(CartAddressType::class, $address);
+        $form ->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($address);
+            $em->flush();
+        }
+        
+        return $this->render('cart/address.html.twig', [
+            'CartAddressType' => $form->createView(),
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -45,6 +45,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $lastname;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $phonenumber;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -154,6 +159,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getPhonenumber(): ?int
+    {
+        return $this->phonenumber;
+    }
+
+    public function setPhonenumber(int $phonenumber): self
+    {
+        $this->phonenumber = $phonenumber;
 
         return $this;
     }
