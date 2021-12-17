057a3d17a0ca6021678d9891baea105336738a2a
terryaki75 <chanterry85@gmail.com>
2021-12-17 09:58:19 +0100

    Modification templates

---
+++ migrations/Version20211210160455.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211210160455 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE produit_categorie (produit_id INT NOT NULL, categorie_id INT NOT NULL, INDEX IDX_CDEA88D8F347EFB (produit_id), INDEX IDX_CDEA88D8BCF5E72D (categorie_id), PRIMARY KEY(produit_id, categorie_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE produit_categorie ADD CONSTRAINT FK_CDEA88D8F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE produit_categorie ADD CONSTRAINT FK_CDEA88D8BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE adresse ADD user_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE adresse ADD CONSTRAINT FK_C35F0816A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C35F0816A76ED395 ON adresse (user_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE adresse DROP FOREIGN KEY FK_C35F0816A76ED395');
+        $this->addSql('DROP TABLE produit_categorie');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP INDEX UNIQ_C35F0816A76ED395 ON adresse');
+        $this->addSql('ALTER TABLE adresse DROP user_id');
+    }
+}
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/edit", name="edit")
      */
-    public function index(): Response
+    public function editInfo(UserInterface $userinterface, Request $request): Response
     {
-        return $this->render('user/index.html.twig', [
+        $form = $this->createForm(RegistrationFormType::class, $userinterface);
+        $form->handleRequest($request);
+
+        return $this->render('user/edit.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    public function editAddress(UserInterface $userinterface, Request $request): Response
+    {
+        $form = $this->createForm(RegistrationFormType::class, $userinterface);
+        $form->handleRequest($request);
+
+        return $this->render('user/edit.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/confirmation_inscription", name="confirmation_inscription")
+     */
+    public function inscription(): Response
+    {
+        return $this->render('user/confirmation_inscription.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
+    
 }
