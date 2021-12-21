7e60764e3026518405c85b2358c2c70ab4850d0c
terryaki75 <95472500+terryaki75@users.noreply.github.com>
2021-12-21 16:51:58 +0100

    ajout react

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+import './styles/app.css';
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App(){
+
+
+    return (
+        <div>Je suis un composant React</div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
