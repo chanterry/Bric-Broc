f15fd20b2d82153a3a14ab4f8057195df27adb7d
terryaki75 <95472500+terryaki75@users.noreply.github.com>
2021-12-21 11:39:35 +0100

    branche bdd

--- src/Controller/Admin/ProduitCrudController.php
+++ src/Controller/Admin/ProduitCrudController.php
@@ -3,8 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Produit;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use App\Entity\Categorie;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField as ImageFields;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 
 class ProduitCrudController extends AbstractCrudController
 {
@@ -14,13 +24,18 @@ class ProduitCrudController extends AbstractCrudController
     }
 
     
-   /* public function configureFields(string $pageName): iterable
+    public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
             TextEditorField::new('description'),
+            ImageField::new('photo')
+            ->setBasePath(' uploads/')
+            ->setUploadDir('public/uploads')
+            ->setFormType(FileUploadType::class)
+            ->setUploadedFileNamePattern('[randomhash].[extension]')
+            ->setRequired(false),
         ];
     }
-    */
+    
 }
